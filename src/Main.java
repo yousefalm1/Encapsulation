1d81d0184d240fd96143a4c9165c55862bb745b6
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-05 10:02:23 +0300

    Finish Task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Yousef", 42, 3.0);
+
+//        System.out.println(student);
+
+
+//        System.out.println(student.getGpaStatus());
+        student.setGpa(4.1);
+        System.out.println(student.getGpaStatus());
+
+        ;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
-
 public class Student {
+
+    private String name;
+    private int age;
+    private double gpa;
+
+    public Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public String getGpaStatus() {
+        if (gpa > 3) {
+            return "Excellent";
+        } else if (gpa >= 2 || gpa <= 3) {
+            return "Good";
+        } else {
+            return "Needs Improvement";
+        }
+    }
+
+    public void setGpa(double gpa) {
+
+        if (gpa >= 0 && gpa <= 4) {
+            this.gpa = gpa;
+        } else {
+            System.err.println("Error");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gpa=" + gpa +
+                '}';
+    }
 }
